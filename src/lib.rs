bdc147172be5ff4d852a31d761b3a6affd0e20ed
Akshat Jaimini <destrex271@gmail.com>
2023-04-09 02:12:21 +0530

    Completed

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,42 @@
 extern crate sysinfo;
+use reqwest::Client;
+use serde::Serialize;
+use serde_json::json;
 use std::{collections::HashMap, rc::Rc};
-
 use sysinfo::{Cpu, DiskUsage, Pid, ProcessExt, System, SystemExt};
 
-#[derive(Debug)]
+pub async fn send_data_to_django(
+    sysinfo: SystemInfo,
+    raminfo: Raminfo,
+    prcs: Vec<Proc>,
+    tp_inf: TempInfo,
+) -> Result<(), ()> {
+    let client = Client::new();
+    let url1 = "http://127.0.0.1:8000/items/temp";
+    let url2 = "http://127.0.0.1:8000/items/sys";
+    let url3 = "http://127.0.0.1:8000/items/ram";
+    let url4 = "http://127.0.0.1:8000/items/processes";
+    let payload1 = json!(tp_inf);
+    let payload2 = json!(sysinfo);
+    let payload3 = json!(raminfo);
+    let payload4 = json!(prcs[0]);
+    println!("{:?}", payload4);
+    let response1 = client.post(url1).json(&payload1).send().await.unwrap();
+    let response2 = client.post(url2).json(&payload2).send().await.unwrap();
+    let response3 = client.post(url3).json(&payload3).send().await.unwrap();
+    let response4 = client.post(url4).json(&payload4).send().await.unwrap();
+    if response1.status().is_success()
+        && response2.status().is_success()
+        && response3.status().is_success()
+        && response4.status().is_success()
+    {
+        Ok(())
+    } else {
+        Err(())
+    }
+}
+
+#[derive(Debug, Serialize)]
 pub struct Raminfo {
     used_memory: u64,
     total_memory: u64,
@@ -11,26 +44,33 @@ pub struct Raminfo {
     total_swap: u64,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 pub struct Proc {
-    pid: Pid,
+    pid: String,
     name: String,
-    disk_usage: DiskUsage,
+    written_bytes: u64,
+    total_written_bytes: u64,
+    read_bytes: u64,
+    total_read_bytes: u64,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
+pub struct ProcList {
+    li: Vec<Proc>,
+}
+
+#[derive(Debug, Serialize)]
 pub struct TempInfo {
     values: Vec<String>,
 }
 
-#[derive(Debug)]
-pub struct SystemInfo<'a> {
+#[derive(Debug, Serialize)]
+pub struct SystemInfo {
     name: String,
     kernel_version: String,
     host_name: String,
     os_version: String,
     cpu_count: usize,
-    cpus: Rc<&'a [Cpu]>,
 }
 
 pub fn get_ram(sys: &mut System) -> Raminfo {
@@ -49,15 +89,19 @@ pub fn get_ram(sys: &mut System) -> Raminfo {
     }
 }
 
-pub fn get_processes(sys: &mut System) -> HashMap<&str, Proc> {
-    let mut map: HashMap<&str, Proc> = HashMap::new();
+pub fn get_processes(sys: &mut System) -> Vec<Proc> {
+    let mut map: Vec<Proc> = vec![];
     for (_, process) in sys.processes() {
+        let disk_us = process.disk_usage();
         let proc: Proc = Proc {
-            pid: process.pid(),
+            pid: String::from(format!("{:?}", process.pid())),
             name: String::from(process.name()),
-            disk_usage: process.disk_usage(),
+            read_bytes: disk_us.read_bytes,
+            total_read_bytes: disk_us.total_read_bytes,
+            written_bytes: disk_us.written_bytes,
+            total_written_bytes: disk_us.total_written_bytes,
         };
-        map.insert(process.name(), proc);
+        map.push(proc);
     }
     map
 }
@@ -73,14 +117,13 @@ pub fn get_temp_info(sys: &mut System) -> TempInfo {
 
 pub fn get_system_info(sys: &mut System) -> SystemInfo {
     sys.refresh_cpu();
-    let cps = Rc::new(sys.cpus());
-    let cpus = cps.to_owned();
+    // let cps = Rc::new(sys.cpus());
+    // let cpus = cps.to_owned();
     SystemInfo {
         name: sys.name().unwrap(),
         kernel_version: sys.kernel_version().unwrap(),
         host_name: sys.host_name().unwrap(),
         os_version: sys.os_version().unwrap(),
         cpu_count: sys.cpus().len(),
-        cpus,
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,23 @@
-use std::thread;
-use sys_monitor::{get_processes, get_ram, get_system_info, get_temp_info};
+use sys_monitor::{get_processes, get_ram, get_system_info, get_temp_info, send_data_to_django};
 use sysinfo::{System, SystemExt};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut sys: System = System::new_all();
     let sys_info = get_system_info(&mut sys);
 
-    let ram_thread = thread::spawn(move || {
-        let mut sysr = System::new_all();
-        get_ram(&mut sysr);
-    });
+    let mut sysr = System::new_all();
+    let ram_data = get_ram(&mut sysr);
 
-    let processes = thread::spawn(move || {
-        let mut sysr = System::new_all();
-        get_processes(&mut sysr);
-    });
+    let mut sysr = System::new_all();
+    let procs = get_processes(&mut sysr);
+    println!("{:?}", procs);
 
-    let temp_thread = thread::spawn(move || {
-        let mut sysr = System::new_all();
-        get_temp_info(&mut sysr);
-    });
+    let mut sysr = System::new_all();
+    let temps = get_temp_info(&mut sysr);
 
-    ram_thread.join().unwrap();
-    processes.join().unwrap();
-    temp_thread.join().unwrap();
+    match send_data_to_django(sys_info, ram_data, procs, temps).await {
+        Ok(()) => println!("Done!"),
+        Err(()) => println!("Error!"),
+    }
 }
