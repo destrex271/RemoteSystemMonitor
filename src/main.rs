35be9b1b94b8bea17e82c0ece2e2541cd5c1ca57
Akshat Jaimini <destrex271@gmail.com>
2023-04-08 23:11:43 +0530

    Threads running

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,22 @@ fn main() {
     let mut sys: System = System::new_all();
     let sys_info = get_system_info(&mut sys);
 
-    let ram_thread = thread::spawn();
+    let ram_thread = thread::spawn(move || {
+        let mut sysr = System::new_all();
+        get_ram(&mut sysr);
+    });
+
+    let processes = thread::spawn(move || {
+        let mut sysr = System::new_all();
+        get_processes(&mut sysr);
+    });
+
+    let temp_thread = thread::spawn(move || {
+        let mut sysr = System::new_all();
+        get_temp_info(&mut sysr);
+    });
+
+    ram_thread.join().unwrap();
+    processes.join().unwrap();
+    temp_thread.join().unwrap();
 }
